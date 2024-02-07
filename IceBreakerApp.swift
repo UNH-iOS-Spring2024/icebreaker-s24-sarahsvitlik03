e7e1fe78472d30d3659c425771735475c07ebae3
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2024-02-07 12:48:18 -0500

    Initial Commit

---
+++ IceBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceBreakerApp.swift
+//  IceBreaker
+//
+//  Created by Sarah Svitlik on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct IceBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
